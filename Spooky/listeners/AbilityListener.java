44f86fdcbaff428e1ea1bafd0aeef2946e81c9e7
ShinyShadow_ <83542827+ShinyShadow12@users.noreply.github.com>
2024-10-23 13:58:42 +0200

    Update AbilityListener.java

--- Spooky/listeners/AbilityListener.java
+++ Spooky/listeners/AbilityListener.java
@@ -2,15 +2,14 @@ package me.ShinyShadow_.Spooky.listeners;
 
 
 import com.projectkorra.projectkorra.BendingPlayer;
-
 import com.projectkorra.projectkorra.ability.CoreAbility;
-import me.ShinyShadow_.Spooky.Spooky;
 import me.ShinyShadow_.Spooky.ability.spookyability.ScareCrow;
 import org.bukkit.block.Block;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class AbilityListener implements Listener {
@@ -23,24 +22,22 @@ public class AbilityListener implements Listener {
         player = event.getPlayer();
         BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
 
-        if(event.getAction() == Action.LEFT_CLICK_BLOCK && !player.isSneaking()) {
-
-            if(bPlayer.canBend(CoreAbility.getAbility(ScareCrow.class)) && bPlayer.getBoundAbilityName().equalsIgnoreCase("ScareCrow") && !ScareCrow.test) {
+        if(event.getAction() == Action.LEFT_CLICK_BLOCK && bPlayer.canBend(CoreAbility.getAbility(ScareCrow.class))
+                && bPlayer.getBoundAbilityName().equalsIgnoreCase("ScareCrow")) {
+            if(!ScareCrow.test  && !player.isSneaking()) {
                     scareCrowPos = event.getClickedBlock();
                     new ScareCrow(player, scareCrowPos);
             }
-        }
-            //End the ability without it getting triggered so you can replace it
-        else if(event.getAction() == Action.LEFT_CLICK_BLOCK && player.isSneaking()) {
-                ScareCrow.stop()
+            else if(ScareCrow.test  && player.isSneaking()) {
+                ScareCrow.earlyRemove = true;
+            }
         }
     }
-    //Prevent pumpkin from being broken
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
-            event.getPlayer()
-                if(event.getBlock().getLocation().equals(ScareCrow.pumpkinspawn.getLocation())) {
-                    event.setCancelled(true)
+            event.getPlayer();
+                if(event.getBlock().getLocation().equals(ScareCrow.pumpkinSpawn.getLocation())) {
+                    event.setCancelled(true);
             }
     }
     
